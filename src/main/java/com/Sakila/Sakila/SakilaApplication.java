a207f8b4804ada33e13989c2a214837e9066fd7a
Yuge Sun <sun.yuge@hotmail.com>
2022-10-31 18:52:26 +0000

    added delete mapping

--- src/main/java/com/Sakila/Sakila/SakilaApplication.java
+++ src/main/java/com/Sakila/Sakila/SakilaApplication.java
@@ -3,8 +3,11 @@ package com.Sakila.Sakila;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -37,5 +40,26 @@ public class SakilaApplication {
 	public actor createActor(@RequestBody actor actor) {
 		return actorRepository.save(actor);
 	}
+		@PutMapping("/putActor/{id}")
+	public ResponseEntity<actor> updateActor(@PathVariable(value = "id") Integer id, @RequestBody actor actorDetails) {
+		Optional<actor> optionalActor =  actorRepository.findById(id);
+		actor actor = optionalActor.get();
+		actorDetails.setActor_id(actorDetails.getActor_id());
+		actorDetails.setLast_name(actorDetails.getLast_name());
+		actorDetails.setFirst_name(actorDetails.getFirst_name());
+		actor updatedActor = actorRepository.save(actorDetails);
+		return ResponseEntity.ok(updatedActor);
+	}
+	@DeleteMapping("/delete/{id}")
+	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Integer id){
+		Optional<actor> optionalActor =  actorRepository.findById(id);
+		actor actor = optionalActor.get();
+		actorRepository.delete(actor);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+	}
+
+
 
 }
--- src/main/java/com/Sakila/Sakila/actor.java
+++ src/main/java/com/Sakila/Sakila/actor.java
@@ -36,7 +36,7 @@ public class actor {
         return last_name;
     }
 
-    public void setlast_name(String last_name) {
+    public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
 
